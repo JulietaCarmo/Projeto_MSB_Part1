ebf933eb394ee398a2835ffab803bcb9f701066e
JulietaCarmo <ju.reis.trabalho18@gmail.com>
2026-05-21 00:39:37 +0100

    KEGG-GSE and ORA enrichGO

--- projeto_JU.R
+++ projeto_JU.R
@@ -258,6 +258,7 @@ sig_results[["T-Cells"]]
 sig_results[["Macrophage1"]]
 
 saveRDS(seurat_obj, file = "1_05_26.rds")
+saveRDS(markers.celulas, file = "markers.celulas.rds")
 
 ###############################
 ################ GSE for one cell type
@@ -270,6 +271,8 @@ library(org.Hs.eg.db)
 organism = "org.Hs.eg.db"
 # Escolhi este tipo de celulas pq foi o que tinhas usado antes
 celltype <- "BAL-Monocytes"
+
+markers.celulas <- readRDS("markers.celulas.rds")
 
 # differential-expression table for BAL-Monocytes
 de <- markers.celulas[[celltype]]
@@ -309,18 +312,303 @@ saveRDS(gse, file = "gse_Monocytes.rds")
 gse_Mono <- readRDS("gse_Monocytes.rds")
 
 # plots
-dotplot(gse, showCategory = 10, split = ".sign") + facet_grid(. ~ .sign)
+dotplot(gse_Mono, showCategory = 10, split = ".sign") + facet_grid(. ~ .sign)
 
-x <- pairwise_termsim(gse)
+x <- pairwise_termsim(gse_Mono)
 emapplot(x, showCategory = 10)
 
-ridgeplot(gse) + labs(x = "enrichment distribution")
+ridgeplot(gse_Mono) + labs(x = "enrichment distribution")
 
 # literature trend for top 3 terms
 terms <- gse@result$Description[1:3]
 pmcplot(terms, 2010:2025, proportion = FALSE)
 
 
+## KEGG Gene Set Enrichment Analysis - for one cell type
+# DE table
+
+organism = "org.Hs.eg.db"
+# Escolhi este tipo de celulas pq foi o que tinhas usado antes
+celltype <- "BAL-Monocytes"
+
+markers.celulas <- readRDS("markers.celulas.rds")
+
+de_kegg <- markers.celulas[[celltype]]
+
+head(de_kegg)
+colnames(de_kegg)
+
+# using all genes, not only top 5
+genes_df <- data.frame(SYMBOL = rownames(de_kegg), 
+                       log2FC = de_kegg$avg_log2FC)
+head(genes_df)
+
+# Convert gene IDs for gseKEGG
+gene_ids <- bitr(genes_df$SYMBOL, 
+                 fromType = "SYMBOL", 
+                 toType = "ENTREZID", 
+                 OrgDb = organism)
+# 10.15% of input gene IDs are fail to map
+head(gene_ids)
+
+# Merge IDs with fold changes
+genes_df <- merge(genes_df, gene_ids, by = "SYMBOL")
+head(genes_df)
+
+# Remove rows without log2FoldChange or ENTREZID
+genes_df <- genes_df[!is.na(genes_df$log2FC) &
+                     !is.na(genes_df$ENTREZID), ]
+head(genes_df)
+
+# Create ranked KEGG gene list
+kegg_gene_list <- genes_df$log2FC
+names(kegg_gene_list) <- as.character(genes_df$ENTREZID)
+
+head(kegg_gene_list)
+
+# Remove NA values and sort decreasing
+kegg_gene_list <- na.omit(kegg_gene_list)
+kegg_gene_list <- sort(kegg_gene_list, decreasing = TRUE)
+
+# Diagnostic checks
+length(kegg_gene_list) # [1] 12067
+
+# KEGG GSEA
+sum(duplicated(names(kegg_gene_list))) # [1] 0
+head(kegg_gene_list)
+
+## Create gseKEGG object
+set.seed(123)
+# human KEGG code
+kegg_organism <- "hsa"
+
+kk2 <- gseKEGG(geneList = kegg_gene_list,
+               organism = kegg_organism,
+               minGSSize = 10,   # so that the pathway doesn't look artificially significant
+               maxGSSize = 500,
+               pvalueCutoff = 0.05,
+               verbose = TRUE,
+               pAdjustMethod = "none")
+# There are ties in the preranked stats (13.79% of the list) - acceptable, but not ideal
+# There were 5 pathways for which P-values were not calculated properly
+  # This happens when: 
+    # nearly all genes in a pathway go in one direction
+    # enrichment score becomes unstable
+    # permutation statistics fail
+  # Very common in single-cell analyses.
+# Invalid p-values detected --> result of the previous errors
+
+
+head(kk2, 10)
+head(kk2@result)
+
+# remove NA p-values directly in the object
+kk2@result <- kk2@result[!is.na(kk2@result$pvalue), ]
+
+# DotPlot
+dotplot(kk2, showCategory = 10, title = "Enriched Pathways" , 
+        split=".sign") + facet_grid(.~.sign)
+
+# Encrichment plot map
+x2 <- pairwise_termsim(kk2) 
+emapplot(x2)
+
+# Category Netplot
+cnetplot(kk2, foldChange = kegg_gene_list)
+
+# Ridgeplot
+ridgeplot(kk2) + labs(x = "enrichment distribution")
+
+# GSEA Plot
+gseaplot(kk2, by = "all", title = kk2$Description[1], geneSetID = 1)
+
+# Pathview
+library(pathview)
+kk2@result$ID # [1] "hsa04668" "hsa05323" "hsa00513" "hsa04657" "hsa05134" "hsa05140"
+
+# Produce the native KEGG plot (PNG)
+dme <- pathview(gene.data = kegg_gene_list, pathway.id = "hsa04668", species = kegg_organism)
+
+# Produce a different plot (PDF) (not displayed here)
+dme <- pathview(gene.data = kegg_gene_list, pathway.id = "hsa04668", species = kegg_organism, kegg.native = F)
+
+
+## Over representation analysis (ORA)
+organism = "org.Hs.eg.db"
+celltype <- "BAL-Monocytes"
+markers.celulas <- readRDS("markers.celulas.rds")
+
+# differential-expression table for BAL-Monocytes
+de_ora <- markers.celulas[[celltype]]
+de_ora_5 <- head(de_ora, n=5)
+
+# inspect columns first
+head(de_ora)
+colnames(de_ora)
+
+head(de_ora_5)
+colnames(de_ora_5)
+
+# build ranked gene list
+# sometimes you may have avg_log2FC, sometimes log2FoldChange-like output
+gene_list <- de_ora$avg_log2FC
+names(gene_list) <- rownames(de_ora)
+
+# remove NA and sort
+gene_list <- na.omit(gene_list)
+gene_list <- sort(gene_list, decreasing = TRUE)
+
+head(gene_list)
+head(names(gene_list))
+
+# ora - extract significant genes only
+# padj ---> p_val_adj
+sig_genes_df <- subset(de_ora, p_val < 0.05 & 
+                         abs(avg_log2FC) > 0.25)
+# abs(avg_log2FC) > 2 returns nothing, so I tried > 0.25 and > 0.1
+  # with > 0.25 and > 0.1 --> only got 1 gene --> so change p_val_adj to p_val
+# Name the vector and omit NA values
+genes <- rownames(sig_genes_df)
+genes <- na.omit(genes)
+
+length(genes) # [1] 1 ---> [1] 691
+head(genes) # [1] "SEMA6B" ---> [1] "SEMA6B"   "HPGDS"    "RASGEF1B" "INSIG1"   "GLO1"     "ITGAE"
+
+# Create enrichGO object
+eGO_Monocyte <- enrichGO(gene = genes,
+                      universe = names(gene_list),
+                      OrgDb = organism, 
+                      keyType = 'SYMBOL',
+                      readable = T,
+                      ont = "BP",
+                      pAdjustMethod = "BH",
+                      pvalueCutoff = 0.05, 
+                      qvalueCutoff = 0.10)
+# 10.15% of input gene IDs are fail to map...
+head(eGO_Monocyte)
+
+dotplot(eGO_Monocyte, showCategory = 10)
+
+# Upset Plot
+upsetplot(eGO_Monocyte)
+
+# Bar plot
+barplot(eGO_Monocyte, drop = TRUE, showCategory = 10, 
+        title = "GO Biological Pathways", font.size = 8)
+
+# Encrichment plot map
+y <- pairwise_termsim(eGO_Monocyte) 
+emapplot(y)
+
+# Category Netplot
+cnetplot(eGO_Monocyte, foldChange = gene_list)
+
+# NOT DONE!!!
+##### ORA - KEGG Pathway Enrichment
+organism = "org.Hs.eg.db"
+# Escolhi este tipo de celulas pq foi o que tinhas usado antes
+celltype <- "BAL-Monocytes"
+
+markers.celulas <- readRDS("markers.celulas.rds")
+
+de_kegg <- markers.celulas[[celltype]]
+
+head(de_kegg)
+colnames(de_kegg)
+
+# using all genes, not only top 5
+genes_df <- data.frame(SYMBOL = rownames(de_kegg), 
+                       log2FC = de_kegg$avg_log2FC)
+head(genes_df)
+
+# Convert gene IDs for gseKEGG
+gene_ids <- bitr(genes_df$SYMBOL, 
+                 fromType = "SYMBOL", 
+                 toType = "ENTREZID", 
+                 OrgDb = organism)
+# 10.15% of input gene IDs are fail to map
+head(gene_ids)
+
+# Merge IDs with fold changes
+genes_df <- merge(genes_df, gene_ids, by = "SYMBOL")
+head(genes_df)
+
+# Remove rows without log2FoldChange or ENTREZID
+genes_df <- genes_df[!is.na(genes_df$log2FC) &
+                       !is.na(genes_df$ENTREZID), ]
+head(genes_df)
+
+# Create ranked KEGG gene list
+kegg_gene_list <- genes_df$log2FC
+names(kegg_gene_list) <- as.character(genes_df$ENTREZID)
+
+head(kegg_gene_list)
+
+# Remove NA values and sort decreasing
+kegg_gene_list <- na.omit(kegg_gene_list)
+kegg_gene_list <- sort(kegg_gene_list, decreasing = TRUE)
+
+# Diagnostic checks
+length(kegg_gene_list) # [1] 12067
+
+# KEGG GSEA
+sum(duplicated(names(kegg_gene_list))) # [1] 0
+head(kegg_gene_list)
+
+## Create gseKEGG object
+set.seed(123)
+# human KEGG code
+kegg_organism <- "hsa"
+
+kk2 <- gseKEGG(geneList = kegg_gene_list,
+               organism = kegg_organism,
+               minGSSize = 10,   # so that the pathway doesn't look artificially significant
+               maxGSSize = 500,
+               pvalueCutoff = 0.05,
+               verbose = TRUE,
+               pAdjustMethod = "none")
+# There are ties in the preranked stats (13.79% of the list) - acceptable, but not ideal
+# There were 5 pathways for which P-values were not calculated properly
+# This happens when: 
+# nearly all genes in a pathway go in one direction
+# enrichment score becomes unstable
+# permutation statistics fail
+# Very common in single-cell analyses.
+# Invalid p-values detected --> result of the previous errors
+
+
+head(kk2, 10)
+head(kk2@result)
+
+# remove NA p-values directly in the object
+kk2@result <- kk2@result[!is.na(kk2@result$pvalue), ]
+
+# DotPlot
+dotplot(kk2, showCategory = 10, title = "Enriched Pathways" , 
+        split=".sign") + facet_grid(.~.sign)
+
+# Encrichment plot map
+x2 <- pairwise_termsim(kk2) 
+emapplot(x2)
+
+# Category Netplot
+cnetplot(kk2, foldChange = kegg_gene_list)
+
+# Ridgeplot
+ridgeplot(kk2) + labs(x = "enrichment distribution")
+
+# GSEA Plot
+gseaplot(kk2, by = "all", title = kk2$Description[1], geneSetID = 1)
+
+# Pathview
+library(pathview)
+kk2@result$ID # [1] "hsa04668" "hsa05323" "hsa00513" "hsa04657" "hsa05134" "hsa05140"
+
+# Produce the native KEGG plot (PNG)
+dme <- pathview(gene.data = kegg_gene_list, pathway.id = "hsa04668", species = kegg_organism)
+
+# Produce a different plot (PDF) (not displayed here)
+dme <- pathview(gene.data = kegg_gene_list, pathway.id = "hsa04668", species = kegg_organism, kegg.native = F)
 
 
 
